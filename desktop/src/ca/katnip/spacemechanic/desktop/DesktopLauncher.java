4f9df14229f49cb549886c612ca11ccbb2cf9c49
Nina Satragno <nsatragno@gmail.com>
2020-06-21 15:35:33 -0400

    First commit.

---
+++ desktop/src/ca/katnip/spacemechanic/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package ca.katnip.spacemechanic.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.katnip.spacemechanic.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Space Mechanic";
+		new LwjglApplication(new Main(), config);
+	}
+}
